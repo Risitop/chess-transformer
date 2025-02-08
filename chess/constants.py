8104db83f64ac71b34a97f99aca5cfd7427bd04a
Risitop <aziz.fouche@gmail.com>
2025-02-08 02:02:36 +0100

    feat: move extraction script

---
+++ chess/constants.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+# Path to the root of the project
+ROOT_DIR = Path(__file__).parent.parent
+DATA_DIR = ROOT_DIR / "data"
+SRC_DIR = ROOT_DIR / "src"
+TEST_DIR = ROOT_DIR / "tests"
+
+# Players
+WHITE = 0
+BLACK = 1
